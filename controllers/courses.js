dfbe6c8323ab601f8fb4cdf3d25b77638f211a92
sandeephexa <sandeepkumar.hexaware@gmail.com>
2020-03-28 23:15:21 +0530

    Course CRUD

--- controllers/courses.js
+++ controllers/courses.js
@@ -1,6 +1,7 @@
 const Course = require('../models/Course');
 const errorResponse = require('../utils/errorResponse');
 const asynchHandler = require('../middleware/async');
+const Bootcamp = require('../models/Bootcamp');
 
 // @desc Get All Courses
 // @route GET api.v1/courses
@@ -23,5 +24,91 @@ exports.getCourses = asynchHandler(async (req,res,next) => {
         success : true,
         count : courses.length,
         data : courses
+    })
+})
+
+// @desc Get Single Course
+// @route GET api.v1/courses/:id
+// @access Public
+exports.getCourse = asynchHandler(async (req,res,next) => {
+
+    const course = await Course.findById(req.params.id).populate({
+        path : 'bootcamp',
+        select : 'name description'
+    }); 
+   
+    if(!course){
+        return next(
+            new errorResponse(`No course found with id ${req.params.id}`),404
+        )
+    }
+    res.status(200).json({
+        success : true,
+        data : course
+    })
+})
+
+// @desc Add single Course
+// @route POST api.v1/courses
+// @route POST api.v1/bootcamps/:bootcampId/courses
+// @access Private
+exports.addCourse = asynchHandler(async (req,res,next) => {
+    
+    req.body.bootcamp = req.params.bootcampId;
+    const bootcamp = await Bootcamp.findById(req.params.bootcampId)
+
+    if(!bootcamp){
+        return next(
+            new errorResponse(`No Bootcamp found with id ${req.params.bootcampId}`,404)
+        )
+    }
+
+    const course = await Course.create(req.body);
+
+    res.status(200).json({
+        success : true,
+        data : course
+    })
+})
+
+// @desc update single Course
+// @route PUT api.v1/courses/:id
+// @access Private
+exports.updateCourse = asynchHandler(async (req,res,next) => {
+    
+    let course = await Course.findById(req.params.id)
+
+    if(!course){
+        return next(
+            new errorResponse(`No course found with id ${req.params.id}`,404)
+        )
+    }
+
+    course = await Course.findByIdAndUpdate(req.params.id,req.body,{new : true, runValidators : true});
+
+    res.status(200).json({
+        success : true,
+        data : course
+    })
+})
+
+// @desc delete single Course
+// @route POST api.v1/courses/:id
+// @access Private
+exports.deleteCourse = asynchHandler(async (req,res,next) => {
+    
+    const course = await Course.findById(req.params.id)
+
+    if(!course){
+        return next(
+            new errorResponse(`No course found with id ${req.params.id}`,404)
+        )
+    }
+
+    await course.remove();
+
+    res.status(200).json({
+        success : true,
+        data : {}
     })
 })
--- models/Course.js
+++ models/Course.js
@@ -38,4 +38,41 @@ const CourseSchema = mongoose.Schema({
     }
 });
 
+// Static method to get average of course tuitions
+CourseSchema.statics.getAverageCost = async function(bootcampId){
+    console.log("calculating averge cost ...".blue);
+
+    const obj = await this.aggregate([
+        {
+            $match : {bootcamp : bootcampId}
+        },
+        {
+            $group : {
+                _id : '$bootcamp',
+                averageCost :{ $avg : '$tuition' }
+            }
+        }
+    ])
+
+    console.log(obj);
+
+    try {
+        await this.model('Bootcamp').findByIdAndUpdate(bootcampId,{
+           averageCost : Math.ceil(obj[0].averageCost / 10) * 10
+        })
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+// Call getAvaerageCost after save
+CourseSchema.post('save',function(){
+    this.constructor.getAverageCost(this.bootcamp)
+})
+
+// Call getAvaerageCost before save
+CourseSchema.pre('remove',function(){
+    this.constructor.getAverageCost(this.bootcamp)
+})
+
 module.exports = mongoose.model('Course',CourseSchema);
